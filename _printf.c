fc773c24efde9471353e4bb71b7b0f1c1952c48b
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-03-31 07:25:51 -0700

    prints depending on the format passed

--- _printf.c
+++ _printf.c
@@ -1,10 +1,10 @@
 #include "main.h"
+
 /**
  * _printf - prints depending on the format passed
  * @format: the format specified
  * Return: number of characters printed, or -1 if error
  */
-
 int _printf(const char *format, ...)
 {
 	int count = 0, x = 0, y = 0, z = 0;
@@ -12,22 +12,27 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 	if (format == NULL)
+		/*printf's man says that in error returns a negative value*/
 		return (-1);
 
+	/*at this point, is known that format is not NULL*/
 	while (format[x] != '\0')
 	{
 		if (format[x] == '%')
 		{
+			/*only looking at the next argument, but not moving*/
 			if (format[x + 1] != '\0')
 			{
+				/*goes to get_sp function, executes and stores return in y*/
 				y = get_sp(args, format[x + 1]);
 				count = count + y;
+				/*moves to next argument, the one that was being looked*/
 				x++;
 			}
 			x++;
 		}
-		_write(format[x]);
-		z = _write(format[x]); /* chequear si al guardarlo ya no printea */
+		/*writes while format[x] is not '%' and stores return in z*/
+		z = _write(format[x]);
 		count = count + z;
 		x++;
 	}
